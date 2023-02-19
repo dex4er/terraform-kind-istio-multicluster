367dbb2f37a3192c31f59ca86e3a9520be221fb1
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2023-02-19 16:09:53 +0100

    Use flux command to install it in the cluster

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -1,6 +1,22 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
+provider "registry.terraform.io/hashicorp/local" {
+  version     = "2.3.0"
+  constraints = "2.3.0"
+  hashes = [
+    "h1:+l9ZTDGmGdwnuYI5ftUjwP8UgoLw4f4V9xoCzal4LW0=",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/null" {
+  version     = "3.2.1"
+  constraints = "3.2.1"
+  hashes = [
+    "h1:FbGfc+muBsC17Ohy5g806iuI1hQc4SIexpYCrQHQd8w=",
+  ]
+}
+
 provider "registry.terraform.io/justenwalker/kind" {
   version = "0.17.0"
   hashes = [
--- main.tf
+++ main.tf
@@ -5,7 +5,21 @@ terraform {
       source  = "justenwalker/kind"
       version = "0.17.0"
     }
+    local = {
+      ## https://registry.terraform.io/providers/hashicorp/local/latest/docs
+      source  = "hashicorp/local"
+      version = "2.3.0"
+    }
+    null = {
+      ## https://registry.terraform.io/providers/hashicorp/null/latest/docs
+      source  = "hashicorp/null"
+      version = "3.2.1"
+    }
   }
+}
+
+locals {
+  tool_versions = { for line in split("\n", file(".tool-versions")) : split(" ", line)[0] => split(" ", line)[1] if line != "" }
 }
 
 provider "kind" {
@@ -34,4 +48,19 @@ resource "kind_cluster" "backend" {
       })]
     }]
   })
+}
+
+resource "local_sensitive_file" "kubeconfig" {
+  content  = resource.kind_cluster.backend.kubeconfig
+  filename = ".kube/config"
+}
+
+resource "null_resource" "flux_backend" {
+  triggers = {
+    flux_version = local.tool_versions["flux2"]
+  }
+
+  provisioner "local-exec" {
+    command = "flux install --context kind-backend --kubeconfig .kube/config --version v${local.tool_versions["flux2"]}"
+  }
 }
