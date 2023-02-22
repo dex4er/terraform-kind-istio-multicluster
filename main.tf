c3b17ed29b5e8dbff8b9080c799ccd0ba8d9481a
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2023-02-22 23:05:39 +0100

    Frontend cluster

--- main.tf
+++ main.tf
@@ -68,9 +68,42 @@ resource "kind_cluster" "backend" {
   })
 }
 
-resource "local_sensitive_file" "kubeconfig" {
+resource "kind_cluster" "frontend" {
+  name = "frontend"
+
+  config = yamlencode({
+    kind       = "Cluster"
+    apiVersion = "kind.x-k8s.io/v1alpha4"
+    name       = "frontend"
+    containerdConfigPatches = [join("\n", [
+      "[plugins.\"io.containerd.grpc.v1.cri\".registry.mirrors.\"localhost:5000\"]",
+      "  endpoint = [\"http://kind-registry:5000\"]"
+    ])]
+    featureGates = {
+      KubeletInUserNamespace = true
+    }
+    nodes = [{
+      role = "control-plane"
+      kubeadmConfigPatches = [yamlencode({
+        kind = "InitConfiguration"
+        nodeRegistration = {
+          kubeletExtraArgs = {
+            node-labels = "ingress-ready=true"
+          }
+        }
+      })]
+    }]
+  })
+}
+
+resource "local_sensitive_file" "backend_kubeconfig" {
   content  = resource.kind_cluster.backend.kubeconfig
-  filename = ".kube/config"
+  filename = ".kube/backend-config"
+}
+
+resource "local_sensitive_file" "frontend_kubeconfig" {
+  content  = resource.kind_cluster.frontend.kubeconfig
+  filename = ".kube/frontend-config"
 }
 
 resource "docker_container" "registry" {
@@ -102,17 +135,35 @@ data "docker_network" "kind" {
 }
 
 locals {
-  kind_network                = [for i in data.docker_network.kind.ipam_config : i.subnet if length(regexall(":", i.subnet)) == 0][0]
-  kind_infra_lb_iprange_start = cidrhost(local.kind_network, 251 * 256)
-  kind_infra_lb_iprange_end   = cidrhost(local.kind_network, 251 * 256)
+  kind_network                             = [for i in data.docker_network.kind.ipam_config : i.subnet if length(regexall(":", i.subnet)) == 0][0]
+  kind_backend_lb_iprange_start            = cidrhost(local.kind_network, 251 * 256)
+  kind_backend_lb_iprange_end              = cidrhost(local.kind_network, 251 * 256 + 255)
+  kind_backend_lb_ip_istio_ingressgateway  = cidrhost(local.kind_network, 251 * 256)
+  kind_frontend_lb_iprange_start           = cidrhost(local.kind_network, 252 * 256)
+  kind_frontend_lb_iprange_end             = cidrhost(local.kind_network, 252 * 256 + 255)
+  kind_frontend_lb_ip_istio_ingressgateway = cidrhost(local.kind_network, 252 * 256)
 }
 
-resource "local_file" "env_file" {
+resource "local_file" "backend_env_file" {
   content = join("\n", [
-    "lb_iprange_start=${local.kind_infra_lb_iprange_start}",
-    "lb_iprange_end=${local.kind_infra_lb_iprange_end}",
+    "lb_iprange_start=${local.kind_backend_lb_iprange_start}",
+    "lb_iprange_end=${local.kind_backend_lb_iprange_end}",
+    "lb_ip_istio_ingressgateway=${local.kind_backend_lb_ip_istio_ingressgateway}",
   ])
   filename = "flux/backend/flux-system/cluster-vars.env"
+
+  depends_on = [
+    data.docker_network.kind,
+  ]
+}
+
+resource "local_file" "frontend_env_file" {
+  content = join("\n", [
+    "lb_iprange_start=${local.kind_frontend_lb_iprange_start}",
+    "lb_iprange_end=${local.kind_frontend_lb_iprange_end}",
+    "lb_ip_istio_ingressgateway=${local.kind_frontend_lb_ip_istio_ingressgateway}",
+  ])
+  filename = "flux/frontend/flux-system/cluster-vars.env"
 
   depends_on = [
     data.docker_network.kind,
@@ -131,12 +182,12 @@ resource "null_resource" "flux_push_artifact" {
   }
 
   provisioner "local-exec" {
-    command = "flux push artifact oci://localhost:5000/flux-system:latest --path=flux --source=\"localhost\" --revision=\"$(git rev-parse --short HEAD 2>/dev/null || LC_ALL=C date +%Y%m%d%H%M%S)\""
+    command = "flux push artifact oci://localhost:5000/flux-system:latest --path=flux --source=\"localhost\" --revision=\"$(git rev-parse --short HEAD 2>/dev/null || LC_ALL=C date +%Y%m%d%H%M%S)\" --kubeconfig .kube/backend-config --context kind-backend"
   }
 
   depends_on = [
     docker_container.registry,
-    local_file.env_file,
+    local_file.backend_env_file,
   ]
 }
 
@@ -152,7 +203,13 @@ data "archive_file" "flux-backend-flux-system" {
   output_path = ".flux-backend-flux-system.zip"
 }
 
-resource "null_resource" "flux_system_common_apply" {
+data "archive_file" "flux-frontend-flux-system" {
+  type        = "zip"
+  source_dir  = "flux/frontend/flux-system"
+  output_path = ".flux-frontend-flux-system.zip"
+}
+
+resource "null_resource" "flux_system_backend_common_apply" {
   triggers = {
     flux_version                    = local.tool_versions["flux2"]
     flux_common_directory_checksum  = data.archive_file.flux-common-flux-system.output_base64sha256
@@ -160,11 +217,27 @@ resource "null_resource" "flux_system_common_apply" {
   }
 
   provisioner "local-exec" {
-    command = "kubectl apply -k flux/common/flux-system --server-side"
+    command = "kubectl apply -k flux/common/flux-system --server-side --kubeconfig .kube/backend-config --context kind-backend"
   }
 
   depends_on = [
-    local_sensitive_file.kubeconfig,
+    local_sensitive_file.backend_kubeconfig,
+  ]
+}
+
+resource "null_resource" "flux_system_frontend_common_apply" {
+  triggers = {
+    flux_version                     = local.tool_versions["flux2"]
+    flux_common_directory_checksum   = data.archive_file.flux-common-flux-system.output_base64sha256
+    flux_frontend_directory_checksum = data.archive_file.flux-frontend-flux-system.output_base64sha256
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl apply -k flux/common/flux-system --server-side --kubeconfig .kube/frontend-config --context kind-frontend"
+  }
+
+  depends_on = [
+    local_sensitive_file.frontend_kubeconfig,
   ]
 }
 
@@ -175,23 +248,50 @@ resource "null_resource" "flux_system_backend_apply" {
   }
 
   provisioner "local-exec" {
-    command = "kubectl apply -k flux/backend/flux-system --server-side"
+    command = "kubectl apply -k flux/backend/flux-system --server-side --kubeconfig .kube/backend-config --context kind-backend"
   }
 
   depends_on = [
-    local_sensitive_file.kubeconfig,
-    null_resource.flux_system_common_apply,
+    local_sensitive_file.backend_kubeconfig,
+    null_resource.flux_system_backend_common_apply,
+  ]
+}
+
+resource "null_resource" "flux_system_frontend_apply" {
+  triggers = {
+    flux_version                     = local.tool_versions["flux2"]
+    flux_frontend_directory_checksum = data.archive_file.flux-frontend-flux-system.output_base64sha256
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl apply -k flux/frontend/flux-system --server-side --kubeconfig .kube/frontend-config --context kind-frontend"
+  }
+
+  depends_on = [
+    local_sensitive_file.frontend_kubeconfig,
+    null_resource.flux_system_frontend_common_apply,
   ]
 }
 
 resource "null_resource" "flux_system_kustomization_backend_apply" {
   provisioner "local-exec" {
-    command = "kubectl apply -f flux/backend/flux-system.yaml --server-side"
+    command = "kubectl apply -f flux/backend/flux-system.yaml --server-side --kubeconfig .kube/backend-config --context kind-backend"
   }
 
   depends_on = [
-    local_sensitive_file.kubeconfig,
+    local_sensitive_file.backend_kubeconfig,
     null_resource.flux_system_backend_apply,
+  ]
+}
+
+resource "null_resource" "flux_system_kustomization_frontend_apply" {
+  provisioner "local-exec" {
+    command = "kubectl apply -f flux/frontend/flux-system.yaml --server-side --kubeconfig .kube/frontend-config --context kind-frontend"
+  }
+
+  depends_on = [
+    local_sensitive_file.frontend_kubeconfig,
+    null_resource.flux_system_frontend_apply,
   ]
 }
 
@@ -201,11 +301,26 @@ resource "null_resource" "flux_reconcile_backend" {
   }
 
   provisioner "local-exec" {
-    command = "flux reconcile source oci flux-system && flux reconcile ks flux-system"
+    command = "flux reconcile source oci flux-system --kubeconfig .kube/backend-config --context kind-backend && flux reconcile ks flux-system --kubeconfig .kube/backend-config --context kind-backend"
   }
 
   depends_on = [
     null_resource.flux_system_kustomization_backend_apply,
+    null_resource.flux_push_artifact,
+  ]
+}
+
+resource "null_resource" "flux_reconcile_frontend" {
+  triggers = {
+    flux_push_artifact_id = null_resource.flux_push_artifact.id
+  }
+
+  provisioner "local-exec" {
+    command = "flux reconcile source oci flux-system --kubeconfig .kube/frontend-config --context kind-frontend && flux reconcile ks flux-system --kubeconfig .kube/frontend-config --context kind-frontend"
+  }
+
+  depends_on = [
+    null_resource.flux_system_kustomization_frontend_apply,
     null_resource.flux_push_artifact,
   ]
 }
